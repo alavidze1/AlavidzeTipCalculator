8fe871e792beef19e314f426a235bddcea1abe3b
BT_1N3_04 <sample@bths.edu>
2023-09-21 09:02:44 -0400

    itemsordered

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,18 @@ public class Main {
 
         System.out.println("Please enter the prices of the items you're ordering, type -1 when you've entered them all.");
         double totalCost=0;
-        while(scan.nextDouble()>0){
-            totalCost+=scan.nextDouble();
+        double tempCost=0;
+        double nextItem=scan.nextDouble();
+        scan.nextLine();
+        while(nextItem>=0){
+            tempCost+=((double)((int)((nextItem+0.005)*100)))/100;
+            totalCost=totalCost+tempCost;
+            System.out.println("Tempcost: "+ tempCost);
+            nextItem=scan.nextDouble();
+            scan.nextLine();
+            tempCost=0;
+            System.out.println("Totalcost: "+ totalCost);
+            totalCost+=((double)((int)((totalCost+0.005)*100)))/100;
         }
 
 
