3669afb5e3a6c6cd24d7335fc367264685b7a165
BT_1N3_04 <sample@bths.edu>
2023-09-21 09:21:29 -0400

    some prints done aswell

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         System.out.print("Welcome! How many people are in your group?");
         int numPeople = scan.nextInt();
-        System.out.print("And what is the tip percentage?");
+        System.out.print("And what is the tip percentage?(0-100)");
         int tipPercent = scan.nextInt();
 
         System.out.println("Please enter the prices of the items you're ordering, type -1 when you've entered them all.");
@@ -15,13 +15,20 @@ public class Main {
         while(nextItem>=0){
             tempCost=((double)((int)((nextItem+0.005)*100)))/100;
             totalCost=totalCost+tempCost;
-            System.out.println("Tempcost: "+ tempCost);
+            //System.out.println("Tempcost: "+ tempCost);
             tempCost=0;
             totalCost=((double)((int)((totalCost+0.005)*100)))/100;
-            System.out.println("Totalcost: "+ totalCost);
+            //System.out.println("Totalcost: "+ totalCost);
             nextItem=scan.nextDouble();
             scan.nextLine();
         }
+        System.out.println("The total cost of your order, before tip, is: $" + totalCost);
+        System.out.println("Total Percentage: " + tipPercent + "%" );
+        double totalTip = (totalCost/100)*tipPercent;
+        System.out.println("Total Tip: $" + totalTip);
+        double totalBill = totalCost + totalTip;
+        System.out.println("Total Bill with tip: $" + totalBill);
+        double perPersonBeforeTip = totalCost/numPeople;
 
 
 
